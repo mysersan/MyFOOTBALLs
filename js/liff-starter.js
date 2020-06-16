9074502a8570abe621a5bb462c2029421a012ad7
mysersan <60083537+mysersan@users.noreply.github.com>
2020-06-16 13:00:54 +0700

    LIFF

--- js/liff-starter.js
+++ js/liff-starter.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     const useNodeJS = false;   // if you are not using a node server, set this value to false
-    const defaultLiffId = "1654348268-z7Oq2aj0";   // change the default LIFF value if you are not using a node server
+    const defaultLiffId = "1654349110-Z8mbrQqa";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
     let myLiffId = "";
